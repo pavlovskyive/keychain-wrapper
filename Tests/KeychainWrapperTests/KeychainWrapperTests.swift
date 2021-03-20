f6e10c98fa82c64f4b46a4b7487e47837f89af57
Vsevolod <pavlovskyi.ve@gmail.com>
2021-03-20 21:36:12 +0200

    Keychain wrapper + Tests

--- Tests/KeychainWrapperTests/KeychainWrapperTests.swift
+++ Tests/KeychainWrapperTests/KeychainWrapperTests.swift
@@ -1,15 +1,100 @@
 import XCTest
 @testable import KeychainWrapper
 
+/// IMPORTANT
+///
+/// Launch tests with Mac target selected (iOS keychain works incorrectly in spm unit tests).
+/// Could write mocks, but no time for that now.
+
+
 final class KeychainWrapperTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(KeychainWrapper().text, "Hello, World!")
+    
+    let keychainWrapper = KeychainWrapper()
+    
+    let testKey1 = "testKey1"
+    let testKey2 = "testKey2"
+    
+    let testValue1 = "testValue1"
+    let testValue2 = "testValue2"
+
+    override func tearDown() {
+        
+        try? keychainWrapper.delete(forKey: testKey1)
+        
+        super.tearDown()
+    }
+    
+    func testWrapperSet() {
+        
+        do {
+            try keychainWrapper.set(testValue1, forKey: testKey1)
+            let value = try keychainWrapper.get(forKey: testKey1)
+            XCTAssertEqual(value, testValue1)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+
+    }
+    
+    func testWrapperUpdate() {
+        
+        do {
+            try keychainWrapper.set(testValue1, forKey: testKey1)
+            try keychainWrapper.set(testValue2, forKey: testKey1)
+            let value = try keychainWrapper.get(forKey: testKey1)
+            XCTAssertEqual(value, testValue2)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+        
+        do {
+            try keychainWrapper.set(testValue1, forKey: testKey1)
+            try keychainWrapper.update(testValue2, forKey: testKey1)
+            let value = try keychainWrapper.get(forKey: testKey1)
+            XCTAssertEqual(value, testValue2)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+
+    }
+    
+    func testWrapperDelete() {
+        
+        try? keychainWrapper.set(testValue1, forKey: testKey1)
+        
+        do {
+            try keychainWrapper.delete(forKey: testKey1)
+            let _ = try keychainWrapper.get(forKey: testKey1)
+            XCTFail("Should be an error")
+        } catch {
+            return
+        }
+        
+        try? keychainWrapper.set(testValue1, forKey: testKey1)
+        
+        do {
+            try keychainWrapper.set("", forKey: testKey1)
+            let _ = try keychainWrapper.get(forKey: testKey1)
+            XCTFail("Should be an error")
+        } catch {
+            return
+        }
+        
+    }
+    
+    func testReadError() {
+        
+        do {
+            let _ = try keychainWrapper.get(forKey: testKey1)
+            XCTFail()
+        } catch {
+            return
+        }
+        
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testWrapperSet", testWrapperSet)
     ]
+
 }
